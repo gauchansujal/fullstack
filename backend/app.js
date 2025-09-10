e9e9e5bcc3a4a34a8b798c61a2c1823440c035ba
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-09-11 02:43:33 +0545

    add

--- backend/app.js
+++ backend/app.js
@@ -1,56 +1,14 @@
-const express = require('express');
-const app = express();
-app.use(express.json()); // to handle JSON body
+const http = require('http');
 
-// Example data
-let users = [
-  { id: 1, name: "Sujal", age: 20 },
-  { id: 2, name: "Alex", age: 25 }
-];
-
-// 1. GET → Read all users
-app.get('/', (req, res) => {
-  res.json(users);
+const server = http.createServer((req, res)=>{
+  res.writeHead(200, {
+    'Content-type': 'text/html',
+    'X-Powered-By': 'Node.js',
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Set-Cookie': 'sessionid=abc123; HttpOnly'
+  });
+  res.end('<h1>hello world</h1>');
 });
-
-// 2. POST → Create new user
-app.post('/', (req, res) => {
-  const newUser = { id: users.length + 1, ...req.body };
-  users.push(newUser);
-  res.status(201).json(newUser);
+server.listen(3000, ()=>{
+  console.log('server is running in http://localhost:3000/');
 });
-
-// 3. PUT → Replace user
-app.put('/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  users = users.map(u => u.id === id ? { id, ...req.body } : u);
-  res.json({ message: "User replaced" });
-});
-
-// 4. PATCH → Update user partially
-app.patch('/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  users = users.map(u => u.id === id ? { ...u, ...req.body } : u);
-  res.json({ message: "Users updated" });
-});
-
-// 5. DELETE → Remove user
-app.delete('/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  users = users.filter(u => u.id !== id);
-  res.json({ message: "User deleted" });
-});
-
-// 6. HEAD → Only headers
-app.head('/', (req, res) => {
-  res.set('X-Custom-Header', 'Test');
-  res.status(200).end();
-});
-
-// 7. OPTIONS → Allowed methods
-app.options('/', (req, res) => {
-  res.set('Allow', 'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
-  res.send();
-});
-
-app.listen(3000, () => console.log("✅ Server running at http://localhost:3000"));

1abcbd2ff0613972af21c045207c4f4454ea67e2
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-09-22 10:26:32 +0545

    added

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
-const fs = require('path');
-const pathInfo = path.parse('/users/docs/file.txt');
-console.log(pathInfo);
+const path = requyire('path');
+const pathString1 = path.format({
+    dir: '/users/docs',
+    base: 'file.txt'
+});
 
-console.log('directory : ', pathInfo.dir);
-console.log('filename:', pathInfo.base);
-console.log('Name only:',pathInfo.name);
-console.log('Extension:', pathInfo.ext);
+console.log(pathString1);
+
+const pathString2 = path.format({
+    root : '/',
+    dir : '/users/docs',
+    name : 'file',
+    ext : '.txt'
+});
+
+console.log(pathString2);
+
+const parsePath = path.parse('/users/docs/old-file.txt');
+parsedPath.base = 'new-file.md';
+const newPath = path.format(parsePath);
+console.log(newPath);

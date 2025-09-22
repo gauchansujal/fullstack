6abd79077539999fd116f0990a56f4e194039241
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-09-22 11:14:53 +0545

    finding relative paths

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const path = require('path');
+console.log(path.relative('/users/docs/file.txt','/users/images/photo.jpg'));
 
-console.log(path.normalize('/users/./docs/../data/file.txt'));
+console.log(path.relative('/users/docs/file.txt', '/users/docs/file2.txt'));
 
-console.log(path.normalize('/users//docs////file.txt'));
+console.log(path.relative('/users/doc/file.txt', 'c:\\user\\impl\\bbb'));
 
-console.log(path.normalize('c:\\users\\docs\\..\\file.txt'));
+const absolutePath = '/var/www/static/images/logo.png';
+const webRoot = '/var/www';
+const webPath = path.relative(webRoot, absolutePath).replace(/\\/g, '/');
 
-console.log(path.normalize('.'));
-console.log(path.normalize('..'));
-console.log(path.normalize('/..'));
+console.log(webPath);

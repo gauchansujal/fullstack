fd7c6d09678fb4723896145ad433c780114b8e31
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-09-26 10:13:08 +0545

    os.cpus()

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,7 @@
 const os = require('os');
-console.log(`CPU Architecture: ${os.arch()}`);
+console.log(`cpu Architecture: ${os.arch()}`);
+
+const cpus = os.cpus();
+console.log(`CPU Model: ${cpus[0].model}`);
+console.log(`CPU sPEED : ${cpus[0].speed} MHZ`);
+console.log(`Number of Cores: ${cpus.length}`)

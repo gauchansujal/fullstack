c5b609fad6e4f09b42d4010a117ee8662362976f
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-09-27 10:31:27 +0545

    os.userInfo()

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,16 @@
 const os = require('os');
-console.log(`kernal version: ${os.version()}`);
+const user = os.userInfo();
+console.log('user information:');
+console.log(`-Username: ${user.username}`);
+console.log(`-user ID: ${user.uid}`);
+console.log(`-Group ID: ${user.gid}`);
+console.log(`- Home Directory:#{user.homedir}`);
+
+if (os.platform()==='win32'){
+    console.log(`-Domain:${user.domain || 'N/A'}`);
+
+}
+
+if (user.shell){
+    console.log(`-default shell:${user.shell}`);
+}

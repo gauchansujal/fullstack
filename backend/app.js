9c4116c69e55977c75386f9b36ab6d3807c06a6f
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-09-27 10:35:57 +0545

    added

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,10 @@
 const os = require('os');
-const user = os.userInfo();
-console.log('user information:');
-console.log(`-Username: ${user.username}`);
-console.log(`-user ID: ${user.uid}`);
-console.log(`-Group ID: ${user.gid}`);
-console.log(`- Home Directory:#{user.homedir}`);
+const path = require('path');
 
-if (os.platform()==='win32'){
-    console.log(`-Domain:${user.domain || 'N/A'}`);
+// Get the home directory
+const homeDir = os.homedir();
+console.log(`Home Directory: ${homeDir}`);
 
-}
-
-if (user.shell){
-    console.log(`-default shell:${user.shell}`);
-}
+// Example: Create a path to a config file in the user's home directory
+const configPath = path.join(homeDir, '.myapp', 'config.json');
+console.log(`Config file will be saved to: ${configPath}`);

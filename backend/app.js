a6b4150240a22eaf64297597fd714e68d770fbcc
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-09-28 09:33:45 +0545

    os.tmpdir()

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,3 @@
 const os = require('os');
-const path = require('path');
 
-// Get the home directory
-const homeDir = os.homedir();
-console.log(`Home Directory: ${homeDir}`);
-
-// Example: Create a path to a config file in the user's home directory
-const configPath = path.join(homeDir, '.myapp', 'config.json');
-console.log(`Config file will be saved to: ${configPath}`);
+console.log(`Temporary Directory: ${os.tmpdir()}`);

b4daa592cf761ff2ee4fb85e19567beee68d6734
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-09-30 10:11:54 +0545

    os.networkInterfaces()

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,37 @@
 const os = require('os');
 
-// Get system uptime in seconds
-const uptime = os.uptime();
-console.log(`System Uptime: ${uptime} seconds`);
+// Get network interfaces
+const networkInterfaces = os.networkInterfaces();
+console.log('Network Interfaces:');
+console.log(JSON.stringify(networkInterfaces, null, 2));
 
-// Format uptime in a more readable way
-const uptimeDays = Math.floor(uptime / (60 * 60 * 24));
-const uptimeHours = Math.floor((uptime % (60 * 60 * 24)) / (60 * 60));
-const uptimeMinutes = Math.floor((uptime % (60 * 60)) / 60);
-const uptimeSeconds = Math.floor(uptime % 60);
+// Iterate through network interfaces
+Object.keys(networkInterfaces).forEach((interfaceName) => {
+  console.log(`\nInterface: ${interfaceName}`);
 
-console.log(`System has been running for: ${uptimeDays} days, ${uptimeHours} hours, ${uptimeMinutes} minutes, ${uptimeSeconds} seconds`);
+  networkInterfaces[interfaceName].forEach((interface) => {
+    console.log(` Address Family: ${interface.family}`);
+    console.log(` IP Address: ${interface.address}`);
+    console.log(` Netmask: ${interface.netmask}`);
+    if (interface.mac) {
+      console.log(` MAC Address: ${interface.mac}`);
+    }
+    console.log(` Internal: ${interface.internal ? 'Yes' : 'No'}`);
+  });
+});
+
+// Function to get primary IPv4 address (non-internal)
+function getPrimaryIPv4Address() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const interface of interfaces[name]) {
+      // Skip internal and non-IPv4 addresses
+      if (!interface.internal && interface.family === 'IPv4') {
+        return interface.address;
+      }
+    }
+  }
+  return 'No IPv4 address found';
+}
+
+console.log(`\nPrimary IPv4 Address: ${getPrimaryIPv4Address()}`);

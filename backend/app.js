116bc2ef4629eb456625f625b4aa6353dcd68212
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-10-03 08:24:34 +0545

    added

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,18 @@
-const { URL, URLSearchParams } = require('url');
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
 
-const myURL = new URL('https://example.com/?name=Kai&age=30');
-const params = new URLSearchParams(myURL.search);
-
-// Get a parameter
-console.log(params.get('name'));
-
-// Add a parameter
-params.append('city', 'Stavanger');
-// Delete a parameter
-params.delete('age');
-// Convert to string
-console.log(params.toString());
+http.createServer(function (req, res) {
+  let q = url.parse(req.url, true);
+  let filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+ 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);

4f9acbc34c68a68ac97a3d290fff52998dde6328
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-10-09 11:54:58 +0545

    buffer model

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,6 @@
-const fs = require('fs');
+const buf = Buffer.from('hello world');
+console.log(buf.toString());
 
-const writableStream = fs.createWriteStream('output.txt');
+console.log(buf[0]);
 
-function writeData() {
-  let i = 100;
-  function write() {
-    let ok = true;
-    do {
-      i--;
-      if (i === 0) {
-        // Last time, close the stream
-        writableStream.write('Last chunk!\n');
-        writableStream.end();
-      } else {
-        // Continue writing data
-        const data = `Data chunk ${i}\n`;
-        // Write and check if we should continue
-        ok = writableStream.write(data);
-      }
-    }
-    while (i > 0 && ok);
-
-    if (i > 0) {
-      // We need to wait for the drain event before writing more
-      writableStream.once('drain', write);
-    }
-  }
-  write();
-}
-
-writeData();
-writableStream.on('finish', () => {
-  console.log('All data written successfully.');
-});
+console.log(buf.length);

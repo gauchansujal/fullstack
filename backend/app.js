7646ec4b23694c7fb8ad8fc908fb045b79760615
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-10-11 13:18:43 +0545

    added

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
-const buffer = Buffer.from('hello, world!');
-
-const slice = buffer.slice(7);
-console.log(slice.toString());
-
-const slice2 = buffer.slice(0, 5);
-console.log(slice2.toString());
-
-slice[0] = 11;
-console.log(slice.toString());
+const buffer = Buffer.from('hello world!');
 console.log(buffer.toString());
+
+console.log(buffer.toString('utf8'));
+
+console.log(buffer.toString('utf8', 0,5));
+
+const hexBuffer = Buffer.from('48656c6c6f', 'hex');
+
+console.log(hexBuffer.toString());
+
+const base64Buffer = Buffer.from('SGVsbG8=', 'base64');
+
+console.log(base64Buffer.toString());

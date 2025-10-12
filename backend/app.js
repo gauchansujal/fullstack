86e842cfc95836d0b3ea9e1cddaa55072ed8dcc9
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-10-12 12:54:37 +0545

    Creating a Hash

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto');
 
-const hash = crypto.createHash('sha256')
-.update('hello, node.js!')
-.digest('hex');
-console.log('SHA-256 Hash:', hash);
+const hash = crypto.createHash('sha256');
+hash.update('Hello, world!');
+
+const digest = hash.digest('hex');
+console.log(digest);

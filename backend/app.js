bbe10565636aeec7117a5437233e571b412162e0
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-10-17 18:16:49 +0545

    added

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,3 @@
- const { setTimeout, setInterval, setImmediate} = require('timers');
+const timers = require('timers');
 
- console.log('starting timers....');
-
- setTimeout(()=>{
-  console.log('this runs after 1 second');
- }, 1000);
-
- let counter = 0;
- const interval = setInterval(()=>{
-  counter++;
-  console.log(`Interval tick ${counter}`);
-  if (counter >= 3) clearInterval(interval);
-
- }, 1000);
-
- setImmediate(()=>{
-  console.log('This runs in the next iteration of the event loop');
- });
- console.log('Timers scheduled');
- 
+const timersPromises = require('timers/promises');

999310c9ef7770cab0e58055e1edda1daad297a3
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-10-17 18:20:23 +0545

    added

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,16 @@
-const timers = require('timers');
+setTimeout(()=>{
+  console.log('this message is displayed after 2 seconds');
 
-const timersPromises = require('timers/promises');
+}, 2000);
+
+setTimeout((name)=>{
+  console.log(`Hello, ${name}!`);
+
+}, 1000, 'World');
+
+const timeoutId = setTimeout(()=>{
+  console.log('this will never be displayed');
+},5000);
+
+clearTimeout(timeoutId);
+console.log('timeout has been cancelled');

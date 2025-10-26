0b499575634d3fe307eb46af121c9c5926f49545
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-10-26 13:00:43 +0545

    added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,35 @@
 const assert = require('assert');
 
-assert.strictEqual(1,1);
+const obj1 = {a: 1, b:{c:2}};
+const obj2 = {a : 1, b:{c:2}};
+const obj3 = {a: '1', b: {c:'2'}};
+
+assert.deepEqual(obj1, obj2);
+assert.deepStrictEqual(obj1, obj2);
+
+assert.deepEqual(obj1, obj2);
+assert.deepStrictEqual(obj1, obj2);
+
+assert.deepEqual(obj1, obj3);
+
 try{
-  assert.strictEqual('1', 1, 'String "1" is not strictly equal to number 1');
-  assert.strictEqual(true, 1, 'true is not strictly equal to 1')
+  assert.deepStrictEqual(obj1, obj3,'objests are not strictly deep-equal');
+
 }catch (err){
+  console.error(`Error: ${err.message}`);
+
+}
+
+const arr1 = [1,2,[3,4]];
+const arr2 = [1,2,[3,4]];
+const arr3 = ['1', '1',['3','4']];
+assert.deepEqual(arr1, arr2);
+assert.deepStrictEqual(arr1, arr2);
+
+assert.deepEqual(arr1,arr3);
+
+try{
+  assert.deepStrictEqual(arr1,arr3, 'Arrays are not strictly deep-equal');
+} catch (err){
   console.error(`Error: ${err.message}`);
 }

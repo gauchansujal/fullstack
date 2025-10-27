a8a3a223c88478b4d9fe1509da791dd1ce1f9205
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-10-27 14:50:03 +0545

    added

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-// Respond to GET request on the root route
-app.get('/', (req, res) => {
-  res.send('GET request to the homepage');
+app.get('/users/:userId/:booksId', (req, res)=>{
+  res.send(`User ID : ${req.params.userId}, Book ID : ${req.params.booksId}`);
+
 });
 
-// Respond to POST request on the root route
-app.post('/', (req, res) => {
-  res.send('POST request to the homepage');
-});
+app.listen(port, ()=>{
+  console.log(`example app listening at http://localhost:${port}`);
 
-
-
-app.use((req, res) => {
-  res.status(404).send('404 - Page not found');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
